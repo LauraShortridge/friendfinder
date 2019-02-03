ad9d8566e1dda2e8093111c959909fb62e6a1728
unknown <veggie_munchiesandcrunchies@hotmail.com>
2019-02-03 12:42:39 -0500

    api page not working

--- 13-express_01-Activities_16-HotRestaurant_Solved_routes_apiRoutes.js
+++ 13-express_01-Activities_16-HotRestaurant_Solved_routes_apiRoutes.js
@@ -1,14 +1,12 @@
-//Your apiRoutes.js file should contain two routes:
-//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-//A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData = require("../data/friends");
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
 
 // ===============================================================================
 // ROUTING
@@ -21,8 +19,12 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
   });
 
   // API POST Requests
@@ -33,24 +35,28 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    if (friendData.length < 5) {
-      friendData.push(req.body);
+  app.post("/api/tables", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
       res.json(true);
     }
     else {
-      alert("It wasn't added");
+      waitListData.push(req.body);
+      res.json(false);
     }
-    });
-
+  });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
-  // Don't worry about it!
+  // Don"t worry about it!
 
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
-    friendData.length = [];
+    tableData.length = [];
+    waitListData.length = [];
 
     res.json({ ok: true });
   });
--- 13-express_01-Activities_16-HotRestaurant_Solved_server.js
+++ 13-express_01-Activities_16-HotRestaurant_Solved_server.js
@@ -14,9 +14,9 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 7070;
+var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing (makes req.body available in your routes; without this, req.body is undefined)
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -26,8 +26,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
